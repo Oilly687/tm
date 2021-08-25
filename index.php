bca5d26d836916420f0b7d54f572055325b48536
Oilly687 <88489649+Oilly687@users.noreply.github.com>
2021-08-25 10:54:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -95,7 +95,7 @@
 $(
     ()=>{
           var plot_data = Object();
-          var xlabel=[];
+          var xlabel=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
           var data_1=[];
           var data_2=[];
           var id_1 = 'myChart';  
